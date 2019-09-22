60d4b2f289e8d1f9dc183c14fbe65b32ad71176b
Jochen Kiemes <jochen@kiemes.de>
2019-09-22 22:08:14 +0200

    load and update existing file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::fs::File;
-use std::io::BufReader;
-use std::io::Read;
+use std::io::Write;
+use std::collections::HashMap;
 
+use log::*;
 use error_chain::*;
 use chrono::NaiveDate;
 use scraper::{Html, Selector};
 
 mod ohlc;
+
+use ohlc::OHLC;
 
 error_chain! {
     foreign_links {
@@ -18,66 +21,121 @@ error_chain! {
 }
 
 
+fn load_file(f: File) -> Result<Vec<(NaiveDate, OHLC)>> {
+    let mut rdr = csv::ReaderBuilder::new().delimiter(b' ').from_reader(f);
+
+    let mut ohlc_data = vec![];
+    for result in rdr.records() {
+        if let Ok(record) = result {
+            let day = NaiveDate::parse_from_str(&record[0], "%Y-%m-%d").unwrap();
+            let open: f32 = record[1].parse().unwrap();
+            let high: f32 = record[2].parse().unwrap();
+            let low: f32 = record[3].parse().unwrap();
+            let close: f32 = record[4].parse().unwrap();
+            let ohlc = OHLC {
+                open,
+                high,
+                low,
+                close,
+            };
+            ohlc_data.push((day, ohlc));
+        }
+        else {
+            warn!("Parse error: {:?}",result);
+        }
+    }
+    Ok(ohlc_data)
+}
+
 fn run() -> Result<()>  {
     println!("Hello, world!");
 
-    let data = reqwest::get("https://www.onvista.de/aktien/kurshistorie.html?ISIN=US2605661048&RANGE=120M")?.text()?;
-    println!("{}",data);
+    let isin = "US2605661048";
 
-    if false {
-        let file = File::open("../dow").unwrap();
-        let mut file = BufReader::new(file);
+    let fname = format!("stock/{}/ohlc.csv",isin);
+    let known_ohlc = match File::open(&fname) {
+        Ok(f) => load_file(f).expect(&format!("Read error on {}",fname)),
+        _ => vec![]
+    };
 
-        let mut doc = String::new();
-        file.read_to_string(&mut doc).unwrap();
-    }
+    let range = match known_ohlc.last() {
+        Some((ref d,_)) => {
+            let today = chrono::Utc::today().naive_local();
+            let days = NaiveDate::signed_duration_since(today,*d).num_days();
+            assert!(days >= 0);
+            println!("{}",days);
+            let months = (days+29) / 30;
+            let months = months.min(120);
+            format!("{}M",months)
+        },
+        _ => "120M".to_string()
+    };
 
-    if true {
-        let doc = data;
-        let selector = Selector::parse("tr").unwrap();
-        let doc = Html::parse_document(&doc);
-        for line in doc.select(&selector) {
-            if line.value().classes().count() == 1 {
-                let mut fields = line.text();
-                let day = NaiveDate::parse_from_str(fields.next().unwrap(), "%d.%m.%y").unwrap();
-                let open: f32 = fields
-                    .next()
-                    .unwrap()
-                    .replace(".", "")
-                    .replace(',', ".")
-                    .parse()
-                    .unwrap();
-                let low: f32 = fields
-                    .next()
-                    .unwrap()
-                    .replace(".", "")
-                    .replace(',', ".")
-                    .parse()
-                    .unwrap();
-                let high: f32 = fields
-                    .next()
-                    .unwrap()
-                    .replace(".", "")
-                    .replace(',', ".")
-                    .parse()
-                    .unwrap();
-                let close: f32 = fields
-                    .next()
-                    .unwrap()
-                    .replace(".", "")
-                    .replace(',', ".")
-                    .parse()
-                    .unwrap();
+    let url = format!("https://www.onvista.de/aktien/kurshistorie.html?ISIN={}&RANGE={}",isin,range);
+    println!("{}",url);
+    let data = reqwest::get(&url)?.text()?;
 
-                let d_ohlc = ohlc::OHLC { open, high, low, close };
-                println!("{} {}", day, d_ohlc);
-            }
+    let mut all_ohlc = known_ohlc.into_iter().collect::<HashMap<_,_>>();
+
+    let doc = data;
+    let selector = Selector::parse("tr").unwrap();
+    let doc = Html::parse_document(&doc);
+    for line in doc.select(&selector) {
+        if line.value().classes().count() == 1 {
+            let mut fields = line.text();
+            let day = NaiveDate::parse_from_str(fields.next().unwrap(), "%d.%m.%y").unwrap();
+            let open: f32 = fields
+                .next()
+                .unwrap()
+                .replace(".", "")
+                .replace(',', ".")
+                .parse()
+                .unwrap();
+            let low: f32 = fields
+                .next()
+                .unwrap()
+                .replace(".", "")
+                .replace(',', ".")
+                .parse()
+                .unwrap();
+            let high: f32 = fields
+                .next()
+                .unwrap()
+                .replace(".", "")
+                .replace(',', ".")
+                .parse()
+                .unwrap();
+            let close: f32 = fields
+                .next()
+                .unwrap()
+                .replace(".", "")
+                .replace(',', ".")
+                .parse()
+                .unwrap();
+
+            let d_ohlc = OHLC { open, high, low, close };
+            println!("{} {}", day, d_ohlc);
+            all_ohlc.insert(day, d_ohlc);
         }
     }
+    
+
+    let mut all_ohlc = all_ohlc.into_iter().collect::<Vec<_>>();
+    all_ohlc.sort_by_key(|e| e.0);
+
+    if let Ok(mut f) = File::create(fname) {
+        for (day,e) in all_ohlc.into_iter() {
+            writeln!(f,"{} {:.5} {:.5} {:.5} {:.5}",
+                     day,e.open,e.high,e.low,e.close)?;
+        }
+    }
+
     Ok(())
 }
 
 fn main() {
+    simple_logger::init_with_level(log::Level::Warn).unwrap();
+
     if let Err(error) = run() {
         match *error.kind() {
             ErrorKind::Io(_) => println!("Standard IO error: {:?}", error),
